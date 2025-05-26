dabd1a31d2002c05ebd639cb4d2ecc16aac635b5
kdj <935637782@QQ.COM>
2025-05-26 22:22:16 +0800

    delete Set Minio Config node

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from .node import *
 
 NODE_CLASS_MAPPINGS = {
-    "Set Minio Config": SetMinioConfig,
+    # "Set Minio Config": SetMinioConfig,
     "Load Image From Minio": LoadImageFromMinio,
     "Save Image To Minio": SaveImageToMinio,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Set Minio Config": "Set Minio Config",
+    # "Set Minio Config": "Set Minio Config",
     "Load Image From Minio": "Load Image From Minio",
     "Save Image To Minio": "Save Image To Minio",
 }
