63c920443c2c7230da9355dad2886cd7ffd837e7
kdj <>
2025-07-14 09:25:52 +0800

    fix: 修复DifyImageDescribe类中文件上传的MIME类型缺失问题

--- node.py
+++ node.py
@@ -354,7 +354,7 @@ class DifyImageDescribe:
         img.save(buffer, "png")
         # with open(file_name,'rb') as file:
         files = {
-            'file': (file_name, buffer),
+            'file': (file_name, buffer, 'image/png'),
             'user': 'comfyui'
         }
 
